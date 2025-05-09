f670b09b57549bb1931f0f54b6297c95128149ea
chawlagaurav <chawlagauravv@gmail.com>
2025-05-10 01:46:41 +0530

    m

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
 
   const handleCopy = () => {
     const text = "chawlagauravv@gmail.com";
-    navigator.clipboard.writeText(text);
+
     setCopied(true);
   };
 
