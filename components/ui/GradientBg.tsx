c84b04e5f43668637cffdc3e9493937745fbeaab
chawlagaurav <chawlagauravv@gmail.com>
2025-05-10 01:43:17 +0530

    [Fix] : Bug Fix 1.6

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -86,7 +86,9 @@ export const BackgroundGradientAnimation = ({
 
   const [isSafari, setIsSafari] = useState(false);
   useEffect(() => {
-    setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
+    if (typeof navigator !== "undefined") {
+      setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
+    }
   }, []);
 
   return (
